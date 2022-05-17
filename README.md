cb93b1a1b770e5eebe1f4854e0d2a2a3c34dcec6
Albert Diones <albert.diones@flexisourceit.com.au>
2022-05-17 14:01:08 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Handbrake auto encode script
+
+
+A handbrake script (video downscaling)
+for batch rescaling (e.g. uploading on
+website with file size limits on uploads)
---
+++ bin/encode.sh
@@ -0,0 +1,20 @@
+
+DIR=$( cd -- "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && cd ..  && pwd )
+
+
+#echo "Working Dir: $DIR"
+
+encodedCount=0
+for video in $DIR/inputs/*.mp4; do
+    echo "Found file: $video"
+    newFileName="$DIR/"$(basename $video)
+    echo "Encoding to $newFileName"
+    HandBrakeCLI -i "$video" -o "$newFileName" -q30 && mv "$video" $DIR/trash/
+    $encodedCount++
+done
+
+if [ $encodedCount==0 ]; then
+    echo "No video files found"
+else
+    echo "Encoded $encodedCount videos"
+fi
