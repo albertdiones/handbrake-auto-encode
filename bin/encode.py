6a852c1773caa259bbbe19164ced3a79e2e81d4c
Albert Diones <albert.diones@flexisourceit.com.au>
2022-05-18 20:51:23 +0800

    Config yaml

--- bin/encode.py
+++ bin/encode.py
@@ -1,31 +1,41 @@
 import os
 import re
+import yaml
 
 directory=os.getcwd()
 
 
 print(f"Working Dir: {directory}")
 
+with open("config.yml", "r") as stream:
+    try:
+        inputsConfig = yaml.safe_load(stream)['video_inputs']
+    except yaml.YAMLError as exception:
+        print(exception)
+
 encodedCount=0
 
-for file in os.listdir(f"{directory}/inputs/"):
+for inputsDir in inputsConfig:
+    quality = inputsConfig[inputsDir]['quality'];
+    print(f"{inputsDir} target quality: {quality}")
+    for file in os.listdir(f"{directory}/{inputsDir}/"):
 
-    if not re.search('\.(mp4|mov|mkv|m4v)$', file):
-        continue
+        if not re.search('\.(mp4|mov|mkv|m4v)$', file):
+            continue
 
-    print(f"Found file: {file}")
+        print(f"Found file: {file}")
 
-    video = f"{directory}/inputs/{file}"
+        video = f"{directory}/inputs/{file}"
 
-    newBaseName = os.path.basename(video)
+        newBaseName = os.path.basename(video)
 
-    newFileName=f"{directory}/{newBaseName}"
+        newFileName=f"{directory}/{newBaseName}"
 
-    print(f"Encoding to {newFileName}")
-    
-    os.system('HandBrakeCLI -i "'+video+'" -o "'+newFileName+'" -q30 && mv "'+video+'" '+directory+'/trash/')
-    encodedCount+=1
-    
+        print(f"Encoding to {newFileName}")
+        
+        os.system('HandBrakeCLI -i "'+video+'" -o "'+newFileName+'" -q' + str(quality) + ' && mv "'+video+'" '+directory+'/trash/')
+        encodedCount+=1
+        
 
 
 if  encodedCount==0:
